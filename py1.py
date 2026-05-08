08103da530fa9ce17e4a65dbecb93eb518c17a3c
sivadharshini112 <siva123dharshini@gmail.com>
2026-05-08 16:13:53 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,50 @@
+name="shivani"
+age=20
+height=120.8
+weight=199
+clg="padmavani clg"
+print(name)
+print(age)
+print(height)
+print(clg)
+print(type(height))
+print(type(name))
+print(id(age))
+
+
+
+
+
+
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+#comparsion operator
+print(a==b)
+print(a>=b)
+print(a!=b)
+#logical operator
+print(a&b)
+print(a|b)
+print(a!=b)
+#Bitwise operator
+print(a^b)
+print(a/b)
+print(a<b)
+print(a>b)
+#Membership operator
+fruits=["apple","banana","cherry"]
+print("apple"in fruits)
+#inditi operator
+a=[1,2,3]
+b=[1,2,3]
+print(a==b)
+print(a is b)
+
+
+
+
