8438209662432eff3735a7458d3c443f70619836
sivadharshini112 <siva123dharshini@gmail.com>
2026-05-08 16:16:19 +0530

    Add files via upload

---
+++ shivani.py
@@ -0,0 +1,50 @@
+'''
+color=(input("Enter your color"))
+if color=="yellow":
+   print("will buy the dress")
+elif color=="red":
+    print("will buy the dress")
+else:
+    print("not buy")'''
+'''    
+for i in range(10):
+    print(i)
+for i in range(2,9):
+    print(i)
+
+
+for  i in range(70,1,-1):
+     print(i)
+for i in range(15):
+    print("hello")
+for i in range(10):
+    print("shivani")'''
+
+
+ 
+
+
+'''
+for i in "shivani":
+   print(i)'''
+
+
+
+'''
+age=int(input("enter the age")
+weight=int(input("enter the weight")
+if age>18:
+    if weight>45:
+           print("you can donate blood")
+           else:
+               print()'''
+
+'''
+for i in range(10):
+    for i in range(10):
+        print("*",end="")
+    print()'''
+
+
+
+
